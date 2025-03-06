e2e3426fdb5b73b0ffdd75f0ab18cf90fa3dabe8
Alex <the.alex.franklin@gmail.com>
2025-03-05 16:49:23 -0800

    realized I didn't need that global.d.ts file

--- src/global.d.ts
+++
@@ -1,8 +0,0 @@
-interface Window {
-    ipcRenderer: {
-      on: (channel: string, listener: (...args: any[]) => void) => void;
-      off: (channel: string, listener: (...args: any[]) => void) => void;
-      send: (channel: string, ...args: any[]) => void;
-      invoke: (channel: string, ...args: any[]) => Promise<any>;
-    };
-  }
